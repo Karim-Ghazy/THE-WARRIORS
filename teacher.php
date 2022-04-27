6ab3786a63146e5cc3f8aaa951104ecb33e351bd
karim <kareem.mohamed18@msa.edu.eg>
2022-04-27 04:30:57 +0200

    teacher

--- teacher.php
+++ teacher.php
@@ -5,7 +5,7 @@ class teacher extends user1{
 public function setexam(){}
 public function recordattendanceforstudents($id){}
 public function correctexam(){ }
-public function setresult(){}
+public function setresult(){ }
 
 public $object_user1 = new user1;
 }
