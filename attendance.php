49f9f132960c066e9a928ec6316ec0560e5166d9
karim <kareem.mohamed18@msa.edu.eg>
2022-04-27 03:53:18 +0200

    att

--- attendance.php
+++ attendance.php
@@ -1,22 +1,13 @@
 <?php
 include_once "date.php";
 class attendance extends date{
-    private $date;
+    
 
     public function checkattendance($id)
     {}
     public function viewattendance($id)
-    {
-    }
+    {}
 
-	function getDate() {
-		return $this->date;
-	}
-	
-	function setDate($date): self {
-		$this->date = $date;
-		return $this;
-	}
 	public $object_date = new date;
 
 }
